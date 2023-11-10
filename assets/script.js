7253516140cd262d8e175573660288d25d9d5634
MiuFujita <149229087+MiuFujita@users.noreply.github.com>
2023-11-10 15:23:17 +0800

    firstcomplete

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,73 @@
-// 初期状態でポップアップを非表示にする
-document.getElementById("popup").style.display = "none";
+var icon = document.getElementById('homeIconIcon');
+var clickable = true;
 
-// ダウンロードボタン1がクリックされたときの処理
-document.getElementById("downloadButton1").addEventListener("click", function(event) {
-    event.preventDefault(); // リンクのデフォルトのクリック動作をキャンセル
+document.getElementById('homeIcon').addEventListener('click', function() {
+    if (clickable) {
+        icon.classList.add('clicked');
+        clickable = false;
 
-    // ポップアップを表示
-    document.getElementById("popup").style.display = "block";
+        setTimeout(function() {
+            icon.classList.remove('clicked');
+            clickable = true;
+        }, 1000);
+    }
+});
 
-    // "はい" ボタンがクリックされたときの処理
-    document.getElementById("confirmDownloadButton").addEventListener("click", function() {
-        // ダウンロードの実行
-        window.location.href = document.getElementById("downloadLink1").href;
+document.addEventListener('DOMContentLoaded', function() {
+    var homeIcon = document.getElementById('homeIcon');
+    var scrollThreshold = 100;
 
-        // ポップアップを非表示にする
-        document.getElementById("popup").style.display = "none";
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > scrollThreshold) {
+            homeIcon.classList.add('visible');
+        } else {
+            homeIcon.classList.remove('visible');
+        }
     });
 
-    // "キャンセル" ボタンがクリックされたときの処理
-    document.getElementById("cancelDownloadButton").addEventListener("click", function() {
-        // ポップアップを非表示にする
-        document.getElementById("popup").style.display = "none";
+    homeIcon.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     });
 });
+
+
+// ハンバーガーメニューの要素を取得
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+// メニューの要素を取得
+const menu = document.querySelector('.menu_btn');
+
+// ハンバーガーメニューをクリックした時の処理
+hamburgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+});
+
+
+  
+function setupDownloadButton(buttonId, linkId) {
+    const downloadButton = document.getElementById(buttonId);
+    const confirmButton = document.getElementById("confirmDownloadButton");
+    const cancelButton = document.getElementById("cancelDownloadButton");
+    const downloadLink = document.getElementById(linkId);
+
+    downloadButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.style.display = "block";
+
+        confirmButton.addEventListener("click", function() {
+            window.location.href = downloadLink.href;
+            popup.style.display = "none";
+        });
+
+        cancelButton.addEventListener("click", function() {
+            popup.style.display = "none";
+        });
+    });
+}
+
+setupDownloadButton("downloadButton1", "downloadLink1");
+setupDownloadButton("downloadButton2", "downloadLink2");
+setupDownloadButton("downloadButton3", "downloadLink3");
